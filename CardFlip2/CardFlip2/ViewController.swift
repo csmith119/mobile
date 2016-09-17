fa412076b9f458558151a26fb85a0d5b7d3680dd
Evan Elkin <eelkin@carpenter-06.elon.edu>
2016-09-17 16:26:55 -0400

    CardFlip2 fixed UI

--- CardFlip2/CardFlip2/ViewController.swift
+++ CardFlip2/CardFlip2/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cardImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changeImageView(_ sender: UITapGestureRecognizer) {
+        if cardImageView.image == #imageLiteral(resourceName: "Back"){
+            cardImageView.image = #imageLiteral(resourceName: "Face")
+        } else if cardImageView.image == #imageLiteral(resourceName: "Face"){
+            cardImageView.image = #imageLiteral(resourceName: "Back")
+        }
+    }
 
 }
 
