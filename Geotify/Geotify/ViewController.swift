0b5e6b90dc5d60fdc9a1ca79494e0f117cc0c786
Christina Smith <csmith119@carpenter-04.elon.edu>
2016-10-24 02:47:16 -0400

    COmpleted map tutorial

---
+++ Geotify/Geotify/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Geotify
+//
+//  Created by Christina Smith on 10/24/16.
+//  Copyright © 2016 Christina Smith. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
